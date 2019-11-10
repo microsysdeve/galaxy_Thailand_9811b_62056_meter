6b73884511436929e30051e5b64da62d7c9a5d13
microsys <microsys@sina.com>
2019-11-11 07:48:40 +0800

    临时递交

---
+++ deltmp.bat
@@ -0,0 +1,6 @@
+forfiles /p ..\galaxy_Thailand_9811b_62056_meter  /s /m *.l51 /c "cmd /c del /q /f @path"
+forfiles /p ..\galaxy_Thailand_9811b_62056_meter  /s /m *.i /c "cmd /c del /q /f @path"
+
+forfiles /p ..\galaxy_Thailand_9811b_62056_meter  /s /m *.r51 /c "cmd /c del /q /f @path"
+forfiles /p ..\galaxy_Thailand_9811b_62056_meter  /s /m *.pbi /c "cmd /c del /q /f @path"
+forfiles /p ..\galaxy_Thailand_9811b_62056_meter  /s /m *.cout /c "cmd /c del /q /f @path"
